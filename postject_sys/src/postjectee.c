ccc8bfd2c47ae5e7334e35ed0cc197c0c539e713
branchseer <dk4rest@gmail.com>
2024-01-20 15:12:53 +0800

    Test all

--- postject_sys/src/postjectee.c
+++ postject_sys/src/postjectee.c
@@ -11,8 +11,8 @@
 const void *postjectee_find_resource(const char *name, size_t *size,
                                      postjectee_options options) {
   struct postject_options postject_options;
-  postject_options_init(&postject_options);
   postject_options.elf_section_name = options.elf_section_name;
+  postject_options.macho_framework_name = NULL;
   postject_options.macho_section_name = options.macho_section_name;
   postject_options.macho_segment_name = options.macho_segment_name;
   postject_options.pe_resource_name = options.pe_resource_name;
